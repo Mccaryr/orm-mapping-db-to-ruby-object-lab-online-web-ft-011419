313146dca9cf250197ced1d8d76c7fee3084bffd
Robert McCary <robert.mccary@yahoo.com>
2019-02-16 00:53:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,6 +20,14 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    sql = "SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT 1"
+    DB[:conn].execute(sql)
+  end
+
   def self.first_X_students_in_grade_10(x)
     sql = "SELECT *
     FROM students
