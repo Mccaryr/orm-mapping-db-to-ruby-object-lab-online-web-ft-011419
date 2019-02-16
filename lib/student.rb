d9d7f07f4b3ffabaf12b25751efcd79e5f6e3ef5
Robert McCary <robert.mccary@yahoo.com>
2019-02-16 00:39:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,15 +38,15 @@ class Student
   end
 
   def self.students_below_12th_grade
-    sql = <<-SQL 
-    SELECT * 
-    FROM students 
-    WHERE grade < 12 
-    SQL 
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade < 12
+    SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-  end 
+  end
 
   def save
     sql = <<-SQL
