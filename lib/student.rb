37aae52172590fcdfdb428febfcb69bb8b1ab7bb
Robert McCary <robert.mccary@yahoo.com>
2019-02-16 00:57:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,13 @@ class Student
     self.new_from_db(first_student)
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = "SELECT *
+    FROM students
+    WHERE grade = ?"
+    DB[:conn].execute(sql, x)
+  end
+
   def self.first_X_students_in_grade_10(x)
     sql = "SELECT *
     FROM students
