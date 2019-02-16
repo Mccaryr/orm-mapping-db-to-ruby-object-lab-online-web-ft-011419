017cc662ed224e8feb73aa2e0e31b9928729ee89
Robert McCary <robert.mccary@yahoo.com>
2019-02-16 00:37:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,14 +27,13 @@ class Student
   end
 
   def self.all_students_in_grade_9
-    9th_grade = []
     sql = <<-SQL
     SELECT *
     FROM students
     WHERE grade = 9
   SQL
-  DB[:conn].execute(sql,name).map do |row|
-  9th_grade << self.new_from_db(row)
+  DB[:conn].execute(sql).map do |row|
+  self.new_from_db(row)
   end
   end
 
