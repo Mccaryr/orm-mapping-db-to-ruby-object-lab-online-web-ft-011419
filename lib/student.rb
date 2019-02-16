cea4b0e7d2bfad07e3d9d86a5dfd9ca96e399280
Robert McCary <robert.mccary@yahoo.com>
2019-02-16 00:55:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,7 @@ class Student
     WHERE grade = 10
     LIMIT 1"
     first_student = DB[:conn].execute(sql)[0]
+    self.new_from_db(first_student)
   end
 
   def self.first_X_students_in_grade_10(x)
